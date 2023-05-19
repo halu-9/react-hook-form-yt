5afaa7d551da80f7fdac48e3e142b44a3eaa91c9
ryo <>
2023-05-19 22:41:39 +0900

    途中までコミット。（必須化のところ）

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data: any) => console.log(data);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="form-container">
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="名前">名前</label>
+        <input
+          type="text"
+          id="name"
+          {...register("name", { required: true })}
+        />
+        <label htmlFor="メールアドレス">メールアドレス</label>
+        <input type="email" id="email" {...register("email")} />
+        <label htmlFor="パスワード">パスワード</label>
+        <input type="password" id="password" {...register("password")} />
+
+        <button type="submit">送信</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
